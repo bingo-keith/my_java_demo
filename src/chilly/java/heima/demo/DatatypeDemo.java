87983ee71802c62bfd1ec2d4f3f968752f6b5c9c
wenbin.zheng <hg.wb.zheng@gmail.com>
2020-01-20 17:35:24 +0800

    studied number conversion and variable

---
+++ src/chilly/java/heima/demo/DatatypeDemo.java
@@ -0,0 +1,33 @@
+package chilly.java.heima.demo;
+
+public class DatatypeDemo {
+
+	public static void main(String[] args) {
+		/*
+		 * 变量
+		 * 数据类型分类：
+		 * 		1、基本数据类型
+		 * 		2、引用数据类型（类，接口，数值）
+		 * 基本数据类型：4类8种
+		 * 		1、整数			占用字节数
+		 * 			byte			1
+		 * 			short			2
+		 * 			int				4
+		 * 			long			8
+		 * 		2、浮点数
+		 * 			float			4
+		 * 			double			8
+		 * 		3、字符
+		 * 			char			2
+		 * 		4、布尔
+		 * 			boolean			1
+		 * 	整数默认是int类型
+		 * 	浮点数默认是double双精度类型
+		 * 	长整形后缀用L标记
+		 * 	单精度float浮点数用F标记
+		 **/
+		long b = 10000000000000L;
+		System.out.println(b);
+	}
+
+}
